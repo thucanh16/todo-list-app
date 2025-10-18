18deba94231ff1a3fa6be9edeea68cf7884f1abf
thucanh12 <anhanhtk699@gmail.com>
2025-10-18 23:47:00 +0700

    Them func list_task

--- app.py
+++ app.py
@@ -5,8 +5,15 @@ def add_task(task_name):
     tasks.append(task_name) 
     print(f"Đã thêm công việc: '{task_name}'")
 
+def list_tasks(task_name):
+    for i in range(len(task_name)):
+        print(f"{i+1}. {task_name[i]}")
+
+
 # --- Điểm bắt đầu của chương trình --- 
 if __name__ == "__main__": 
     print("Chào mừng đến với ứng dụng To-Do List!") 
     add_task("Học bài Git và GitHub") 
     add_task("Làm bài tập thực hành ở nhà")
+
+    list_tasks(tasks)
